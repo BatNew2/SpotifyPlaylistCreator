4bc99e71cbca72972cf8004ab332220022f92184
BatNew2 <battusainew@gmail.com>
2024-01-04 11:55:34 +0700

    Final

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import os
 SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
 SPOTIPY_SECRET_ID = os.environ.get("SPOTIPY_SECRET_ID")
 SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
-date = input("What date do you want to travel to? Type the date using this format YYYY-MM-DD: ")
+date = input("What date do you want to travel to? Type the date using this format DD-MM-YYYY: ").split("-")
+print("Please wait...")
+date = f"{date[2]}-{date[1]}-{date[0]}"
 link = f"https://www.billboard.com/charts/hot-100/{date}"
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://example.com",
-        client_id="5b97ae9dc315468cb38f89a767630d91",
-        client_secret="0673c582e79d48568a8da692e695e03f",
+        client_id="689c1abe9e7d425ba051cb66ef61305a",
+        client_secret="1158d4e8ed0b416d94132c02cad9a455",
         show_dialog=True,
         cache_path="token.txt",
         username="J"
@@ -33,10 +35,9 @@ else:
     while len(titles) > 50:
         titles.pop(-1)
     titles = [title.replace("\t\t\n\t\n", "") for title in titles]
-    print(len(titles))
     formatted = [sp.search(q=song, limit=1, type="track", market=None) for song in titles]
     formatted = [song["tracks"]["items"][0]["id"] for song in formatted]
     print(formatted)
     playlist = sp.user_playlist_create(user=user_id, name=f"{date} playlist", public="false", description=f"Top songs from {date}\nSome songs may be inaccurate as the names are similar/the same")
     sp.playlist_add_items(playlist_id=playlist["id"], items=formatted, position=None)
-    print("Finished")
+    print(f"Finished, find playlist at {playlist['external_urls']['spotify']}")
